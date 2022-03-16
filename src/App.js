892251068d5e6b8401ad1b0c822b672d785013a2
Isabellukk <isabellukk@gmail.com>
2022-03-15 18:52:33 -0700

    Created the header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from 'react';
+import {BrowserRouter as Router} from 'react-router-dom';
+import JsonData from './data/data.json';
+import {Header} from './components/Header.jsx';
 
-function App() {
+
+
+function App () {
+  const [landingPageData, setLandingPageData] = useState({});
+
+  useEffect(() => {
+    setLandingPageData(JsonData);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <Router>
+      <Header data={landingPageData.Header}/>
+    </Router>);
+};
 
 export default App;
